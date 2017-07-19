7986958f10312260f533d5d765464a63c9906b4d
yuhei ikuzawa <ikuzawayuhei@ikuzawayuutaira-no-MacBook-Air.local>
2017-07-19 13:54:22 +0900

    pusher notice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :current_notifications, if: :signed_in?
+
 
   PERMISSIBLE_ATTRIBUTES = %i(name avatar avatar_cache)
+
+  def current_notifications
+    @notifications_count = Notification.where(user_id: current_user.id).where(read: false).count
+  end
 
   protected
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :blog
   validates :content, presence: true
+  has_many :notifications, dependent: :destroy
 end
