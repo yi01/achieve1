6ba6c0b54874d626c043bd9da325bc77715f724c
yuhei ikuzawa <ikuzawayuhei@ikuzawayuutaira-no-MacBook-Air.local>
2017-09-27 15:59:48 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -17,9 +17,9 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
-  if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: "/letter_opener"
-  end
+  # if Rails.env.development?
+  #   mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  # end
   root 'top#index'
   resources :poems, only: [:index, :show]
   resources :users, only: [:index, :show]
